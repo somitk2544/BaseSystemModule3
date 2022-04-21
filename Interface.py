77e640b90459a0e2b61558cc206745c3c41f21ed
PeaceChanpornpakdee <yothinpeace@gmail.com>
2022-04-21 12:02:06 +0700

    self.os  self.ser  and maxSpeed

--- Interface.py
+++ Interface.py
@@ -9,16 +9,17 @@ import serial
 import math
 import time
 
-os = platform.platform()[0].upper()
-if os == 'M': #Mac
-   ser = serial.Serial('/dev/cu.usbmodem14103', 512000, parity='E', stopbits=1, timeout=1)
-elif os == 'W': #Windows
-   ser = serial.Serial('COM3',512000,parity='E',stopbits=1,timeout=1)
-
 class UserInterface(QWidget):
    def __init__(self):
       super().__init__()
       self.windowInit()
+
+      self.os = platform.platform()[0].upper()
+
+      if self.os == 'M': #Mac
+         self.ser = serial.Serial('/dev/cu.usbmodem14103', 512000, parity='E', stopbits=1, timeout=1)
+      elif self.os == 'W': #Windows
+         self.ser = serial.Serial('COM3',512000,parity='E',stopbits=1,timeout=1)
 
       self.currentStation = 0
       self.currentAngle = 0
@@ -40,31 +41,31 @@ class UserInterface(QWidget):
 
       self.background2 = Text(self, 0, '', 475, 0)
       self.background2.setSize(325, 750)
-      self.background2.object.setPixmap(QPixmap("Desktop/BaseSystemModule3/background.png")) if os == 'M' else self.background2.object.setPixmap(QPixmap("./background.png"))
+      self.background2.object.setPixmap(QPixmap("Desktop/BaseSystemModule3/background.png")) if self.os == 'M' else self.background2.object.setPixmap(QPixmap("./background.png"))
 
       self.title = Text(self, 28, "         BASE SYSTEM         ", 50, 30)
-      self.title.setFontSize(18) if os == 'W' else False
+      self.title.setFontSize(18) if self.os == 'W' else False
       self.title.setStyle("color: white; border-radius: 25; background-color: {}; padding: 10px".format(self.color.darkgray))
       
       self.logBorder = Text(self, 0, '', 36, 135)
       self.logBorder.setSize(400, 42)
       self.logBorder.setStyle("border-radius: 10; border: 3px solid rgb(0, 0, 150); background-color: rgba(255, 255, 255, 200); padding :5px")
       self.logTag = Text(self, 14, "     MESSAGE     ", 175, 125)
-      self.logTag.setFontSize(8) if os == 'W' else False
+      self.logTag.setFontSize(8) if self.os == 'W' else False
       self.logTag.setStyle("color:white; background-color: {}; border-radius: 7; padding :3px".format(self.color.darkblue))
       self.log = Text(self, 15.5, "   PLEASE SELECT 10 SUB-STATIONS   ", 36, 140)
-      self.log.setFontSize(10) if os == 'W' else False
+      self.log.setFontSize(10) if self.os == 'W' else False
       self.log.setSize(400, 40)
       self.log.normal()
       
       self.run = Button(self, 20, "RUN", 50, 670)
-      self.run.setFontSize(12) if os == 'W' else False
+      self.run.setFontSize(12) if self.os == 'W' else False
       self.run.setSize(150, 40)
       self.run.setStyle("color:{}; background-color: {}; border-radius: 10; border: 4px solid {}".format(self.color.white, self.color.darkblue, self.color.darkblue))
       self.run.disable()
 
       self.home = Button(self, 20, "HOME", 250, 670)
-      self.home.setFontSize(12) if os == 'W' else False
+      self.home.setFontSize(12) if self.os == 'W' else False
       self.home.setSize(150, 40)
       self.home.setStyle("color:{}; background-color: {}; border-radius: 10; border: 4px solid {}".format(self.color.white, self.color.lightgray, self.color.lightgray))
       self.home.ready = True
@@ -73,23 +74,23 @@ class UserInterface(QWidget):
       self.speedBorder.setSize(275, 150)
       self.speedBorder.setStyle("border-radius: 15; border: 3px solid white")
       self.speedTag = Text(self, 14, "   SPEED   ", 597, 30)
-      self.speedTag.setFontSize(9) if os == 'W' else False
+      self.speedTag.setFontSize(9) if self.os == 'W' else False
       self.speedTag.setStyle("color:{}; background-color: white; border-radius: 7; border: 3px solid white".format(self.color.darkblue))
       self.maxSpeedText = Text(self, 15, "MAX SPEED\t\tRPM", 530, 70)
-      self.maxSpeedText.setFontSize(9) if os == 'W' else False
+      self.maxSpeedText.setFontSize(9) if self.os == 'W' else False
       self.maxSpeedText.focus()
       self.maxSpeedText.object.setAlignment(Qt.AlignLeft)
       self.maxSpeedInput = InputBox(self, 15, 645, 68)
-      self.maxSpeedInput.setPosition(635, 68) if os == 'W' else False
-      self.maxSpeedInput.setFontSize(9) if os == 'W' else False
+      self.maxSpeedInput.setPosition(635, 68) if self.os == 'W' else False
+      self.maxSpeedInput.setFontSize(9) if self.os == 'W' else False
       self.maxSpeedInput.setSize(40, 25)
       self.maxSpeedInput.focus()
       self.nowSpeedText = Text(self, 15, "NOW SPEED\t0.00\tRPM", 530, 110)
-      self.nowSpeedText.setFontSize(9) if os == 'W' else False
+      self.nowSpeedText.setFontSize(9) if self.os == 'W' else False
       self.nowSpeedText.object.setAlignment(Qt.AlignLeft)
       self.nowSpeedText.focus()
       self.topSpeedText = Text(self, 15, "TOP SPEED\t0.00\tRPM", 530, 150)
-      self.topSpeedText.setFontSize(9) if os == 'W' else False
+      self.topSpeedText.setFontSize(9) if self.os == 'W' else False
       self.topSpeedText.object.setAlignment(Qt.AlignLeft)
       self.topSpeedText.focus()
 
@@ -97,33 +98,33 @@ class UserInterface(QWidget):
       self.goalBorder.setSize(275, 330)
       self.goalBorder.setStyle("border-radius: 15; border: 3px solid white")
       self.goalTag = Text(self, 14, "   GOAL   ", 602, 210)
-      self.goalTag.setFontSize(9) if os == 'W' else False
+      self.goalTag.setFontSize(9) if self.os == 'W' else False
       self.goalTag.setStyle("color:{}; background-color: white; border-radius: 7; border: 3px solid white".format(self.color.darkblue))
       
       self.goalPositionText = Text(self, 15, "ANGULAR\nPOSITION", 540, 253)
-      self.goalPositionText.setFontSize(9) if os == 'W' else False
+      self.goalPositionText.setFontSize(9) if self.os == 'W' else False
       self.goalPositionInput = InputBox(self, 16, 550, 298)
-      self.goalPositionInput.setFontSize(9) if os == 'W' else False
+      self.goalPositionInput.setFontSize(9) if self.os == 'W' else False
       self.goalPositionInput.setSize(50, 25)
       self.goalDegree = Text(self, 12, "o", 605, 295)
-      self.goalDegree.setFontSize(7) if os == 'W' else False
+      self.goalDegree.setFontSize(7) if self.os == 'W' else False
          
       self.goalSingleStationText = Text(self, 15, "SINGLE\nSTATION", 675, 253)
-      self.goalSingleStationText.setFontSize(9) if os == 'W' else False
+      self.goalSingleStationText.setFontSize(9) if self.os == 'W' else False
       self.goalSingleStationInput = InputBox(self, 18, 700, 298)
-      self.goalSingleStationInput.setFontSize(9) if os == 'W' else False
+      self.goalSingleStationInput.setFontSize(9) if self.os == 'W' else False
       self.goalSingleStationInput.setSize(25, 25)
       self.goalSharp = Text(self, 15, "#", 685, 300)
-      self.goalSharp.setFontSize(9) if os == 'W' else False
+      self.goalSharp.setFontSize(9) if self.os == 'W' else False
 
       self.goalMultiStationText = Text(self, 15, "MULTI STATION QUEUE", 552, 370)
-      self.goalMultiStationText.setFontSize(9) if os == 'W' else False
+      self.goalMultiStationText.setFontSize(9) if self.os == 'W' else False
       self.goalMultiStationText.focus()
       self.goalMultiStationInput = []
       for i in range(3):
          for j in range(5):
             self.goalMultiStationInput.append(InputBox(self, 18, 530+48*j, 400+48*i))
-            self.goalMultiStationInput[-1].setFontSize(9) if os == 'W' else False
+            self.goalMultiStationInput[-1].setFontSize(9) if self.os == 'W' else False
             self.goalMultiStationInput[-1].setSize(25, 25)
             self.goalMultiStationInput[-1].disable()
       
@@ -149,16 +150,16 @@ class UserInterface(QWidget):
       self.endEffBorder.setStyle("border-radius: 15; border: 3px solid white")
       self.endEffTag = Text(self, 14, "   END-EFFECTOR   ", 567, 570)
       self.endEffTag.setStyle("color:{}; background-color: white; border-radius: 7; border: 3px solid white".format(self.color.darkblue))
-      self.endEffTag.setFontSize(9) if os == 'W' else False
+      self.endEffTag.setFontSize(9) if self.os == 'W' else False
       self.endEffText = Text(self, 16, "STATUS", 530, 605)
       self.endEffText.setStyle("color:white")
-      self.endEffText.setFontSize(9) if os == 'W' else False
+      self.endEffText.setFontSize(9) if self.os == 'W' else False
       self.endEffToggle = AnimatedToggle(self, checked_color="#44ccff", pulse_checked_color="#0099ccff", pulse_unchecked_color="#00ffffff")
       self.endEffToggle.move(599, 590)
       self.endEffToggle.resize(70, 50)
       self.endEffToggle.toggled.connect(self.endEffControl)
       self.endEffStatusText = Text(self, 16, "DISABLE", 675, 605)
-      self.endEffStatusText.setFontSize(9) if os == 'W' else False
+      self.endEffStatusText.setFontSize(9) if self.os == 'W' else False
       self.endEffStatusText.setStyle("color:gray")
 
       self.mcuStatus = "DISCONNECT"
@@ -166,13 +167,13 @@ class UserInterface(QWidget):
       self.mcuBorder.setSize(275, 60)
       self.mcuBorder.setStyle("border-radius: 15; border: 3px solid white")
       self.mcuTag = Text(self, 14, "   MCU   ", 604, 660)
-      self.mcuTag.setFontSize(9) if os == 'W' else False
+      self.mcuTag.setFontSize(9) if self.os == 'W' else False
       self.mcuTag.setStyle("color:{}; background-color: white; border-radius: 7; border: 3px solid white".format(self.color.darkblue))
       self.mcuText = Text(self, 16, "STATUS", 530, 695)
-      self.mcuText.setFontSize(9) if os == 'W' else False
+      self.mcuText.setFontSize(9) if self.os == 'W' else False
       self.mcuText.setStyle("color:white")
       self.mcuStatusText = Text(self, 15, "DISCONNECT", 665, 694)
-      self.mcuStatusText.setFontSize(9) if os == 'W' else False
+      self.mcuStatusText.setFontSize(9) if self.os == 'W' else False
       self.mcuStatusText.setStyle("color:gray")
       self.mcuStatusText.setSize(100,20)
       self.mcuToggle = AnimatedToggle(self, checked_color="#44ccff", pulse_checked_color="#0099ccff", pulse_unchecked_color="#00ffffff")
@@ -183,9 +184,9 @@ class UserInterface(QWidget):
       
       self.arrow = QLabel(self)
       self.arrow.move(40, 230)
-      if(os == 'M'):
+      if(self.os == 'M'):
          self.arrowImage = QPixmap("Desktop/BaseSystemModule3/arrow.png")
-      elif os == 'W':
+      elif self.os == 'W':
          self.arrowImage = QPixmap("./arrow.png")
       self.arrowImage = self.arrowImage.scaled(390, 390, Qt.KeepAspectRatio)
       self.arrow.setPixmap( self.arrowImage.transformed(QTransform().rotate(0),Qt.SmoothTransformation) )
@@ -258,8 +259,6 @@ class UserInterface(QWidget):
 #-----------------------------------------------------------------------------------------------------------------
 
       self.log.normal()
-      self.log.setText("HELLO")
-
       self.substation = self.station.selected
 
       # if( len(self.station.selected) < 10):
@@ -277,9 +276,9 @@ class UserInterface(QWidget):
             self.log.error()
             self.log.setText("MAX SPEED CANNOT EXCEED 10 RPM")
             self.maxSpeed = -1
-         elif(float(msi) < 5):
+         elif(float(msi) <= 0):
             self.log.error()
-            self.log.setText("MAX SPEED CANNOT BELOW 5 RPM")
+            self.log.setText("MAX SPEED CANNOT BE 0")
             self.maxSpeed = -1
          else:
             self.maxSpeed = msi
@@ -441,10 +440,6 @@ class UserInterface(QWidget):
 
       if(self.running):
          self.mode_10()
-         # if(ser.in_waiting != 0):
-         #    if(ser.read(2) == b'Fn'):
-         #       self.running = False
-         #       print("FINISHED!! Reach Goal\n")
 
 #-----------------------------------------------------------------------------------------------------------------
 
@@ -476,22 +471,22 @@ class UserInterface(QWidget):
       return (~(sum(dataFrame)%256))%256
 
    def serialWait(self):
-      while(ser.in_waiting == 0):
+      while(self.ser.in_waiting == 0):
          pass
 
    def mode_1(self):
       pass
 
    def mode_2(self):
-      ser.write([146,109]) # 10010010 01101101
+      self.ser.write([146,109]) # 10010010 01101101
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Connect MCU")
 
    def mode_3(self):
-      ser.write([147,108]) # 10010011 01101100
+      self.ser.write([147,108]) # 10010011 01101100
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Disconnect MCU")
 
    def mode_4(self):
@@ -499,9 +494,9 @@ class UserInterface(QWidget):
       serialList.append(int(float(self.maxSpeed)*255/10)) # 8 bit (0-255)
       serialList.append(self.checkSum(serialList))
       # print(serialList)
-      ser.write(serialList)
+      self.ser.write(serialList)
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Max Speed : ", self.maxSpeed)
 
    def mode_5(self):
@@ -511,9 +506,9 @@ class UserInterface(QWidget):
       serialList.append(int(goalRad % 256)) # 2nd 8 bit (0-255)
       serialList.append(self.checkSum(serialList))
       # print(serialList)
-      ser.write(serialList)
+      self.ser.write(serialList)
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Goal Position :", self.goalPosition)
 
    def mode_6(self):
@@ -521,9 +516,9 @@ class UserInterface(QWidget):
       serialList.append(int(self.goalSingleStation)) # 1-10
       serialList.append(self.checkSum(serialList))
       # print(serialList)
-      ser.write(serialList)
+      self.ser.write(serialList)
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Goal Single Station :", self.goalSingleStation)
 
    def mode_7(self):
@@ -542,15 +537,15 @@ class UserInterface(QWidget):
       serialList.append(self.checkSum(serialList))
       # print(serialStation)
       # print(serialList)
-      ser.write(serialList)
+      self.ser.write(serialList)
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Goal Multi Station :", self.goalMultiStation[:self.goalMultiStationSize-1])
       
    def mode_8(self):
-      ser.write([152,103]) # 10011000 01100111
+      self.ser.write([152,103]) # 10011000 01100111
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("RUN!! Go to Goal")
       self.running = True
 
@@ -564,33 +559,33 @@ class UserInterface(QWidget):
       print(self.currentStation)
 
    def mode_10(self):
-      ser.write([154,101]) # 10011010 01100101
+      self.ser.write([154,101]) # 10011010 01100101
       self.serialWait()
-      serialRead = ser.read(2)
+      serialRead = self.ser.read(2)
       if(serialRead == b'Xu'):
          serialList = []
          for i in range(4):
-            serialList.append(int.from_bytes(ser.read(1), byteorder='big'))
+            serialList.append(int.from_bytes(self.ser.read(1), byteorder='big'))
          if( self.checkSum(serialList[:-1]) == serialList[-1] and serialList[0] == 154):
             self.currentAngle = serialList[1]*256 + serialList[2]
-            self.currentAngle = self.currentAngle / 31414 * 180
+            self.currentAngle = self.currentAngle / (math.pi * 10000) * 180
             print(self.currentAngle)
-         ser.write([88,117]) # Xu
+         self.ser.write([88,117]) # Xu
 
       elif(serialRead == b'Fn'):
          self.running = False
          print("FINISHED!! Reach Goal\n")
          self.serialWait()
-         serialRead = ser.read(2)
+         serialRead = self.ser.read(2)
          if(serialRead == b'Xu'):
             serialList = []
             for i in range(4):
-               serialList.append(int.from_bytes(ser.read(1), byteorder='big'))
+               serialList.append(int.from_bytes(self.ser.read(1), byteorder='big'))
             if(self.checkSum(serialList[:-1]) == serialList[-1]):
                self.currentAngle = serialList[1]*256 + serialList[2]
-               self.currentAngle = self.currentAngle / 31414 * 180
+               self.currentAngle = self.currentAngle / (math.pi * 10000) * 180
                print(self.currentAngle)
-            ser.write([88,117]) # Xu
+            self.ser.write([88,117]) # Xu
 
    def mode_11(self):
       return
@@ -602,21 +597,21 @@ class UserInterface(QWidget):
       print(self.currentSpeed)
 
    def mode_12(self):
-      ser.write([156,99]) # 10011100 01100011
+      self.ser.write([156,99]) # 10011100 01100011
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Enable End-Effector")
 
    def mode_13(self):
-      ser.write([157,98]) # 10011101 01100010
+      self.ser.write([157,98]) # 10011101 01100010
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Disable End-Effector")
 
    def mode_14(self):
-      ser.write([158,97]) # 10011110 01100001
+      self.ser.write([158,97]) # 10011110 01100001
       self.serialWait()
-      if(ser.read(2) == b'Xu'):
+      if(self.ser.read(2) == b'Xu'):
          print("Set Home")
 
 
